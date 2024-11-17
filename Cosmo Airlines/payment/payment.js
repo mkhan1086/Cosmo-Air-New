99f429910c9efdede22263ee466bda2a3681908b
mkhan1086 <mkhan@bsz.co.za>
2024-11-18 00:04:51 +0200

    Delete Cosmo Airlines directory

--- Cosmo Airlines/payment/payment.js
+++
@@ -1,8 +0,0 @@
-fetch('php/payment.php', {
-    method: 'POST',
-})
-.then((res) => res.json())
-.then((data) => {
-    const stripe = Stripe('your_publishable_key');
-    stripe.redirectToCheckout({ sessionId: data.id });
-});
